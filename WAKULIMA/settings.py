98005ea59bd68ca36bb391db7ca0dd6f35d91fda
Waudo <waudoerick@gmail.com>
2026-01-20 23:26:47 +0300

    Fix ALLOWED_HOST for Render

--- WAKULIMA/settings.py
+++ WAKULIMA/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [27.0.0.1:8000,wakulima.onrender.com]
 
 
 # Application definition
